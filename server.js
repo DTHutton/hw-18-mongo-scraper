24ddfa53dddbcc3b2f42103beed2ec4553a5617a
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-10-09 15:53:51 -0400

    edited public folder, added routes folder, and handlebars folder

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Require all models
+const db = require("./models");
+
+//express 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+
+// morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
