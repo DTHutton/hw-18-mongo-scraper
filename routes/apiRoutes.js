1517201760a84e03c9595841fe0b7fa51e79634a
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-10-10 18:16:18 -0400

    /scrape route pulls title, link, and summary and logs them. doesn't push summary to db with link and title though.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 module.exports = function (app) {
 
@@ -10,18 +10,18 @@ module.exports = function (app) {
         axios.get("https://www.npr.org").then(function (response) {
 
             const $ = cheerio.load(response.data);
+            const results = [];
 
-            $("h3.title").each(function (i, element) {
-
-                const results = [];
+            $("h3.title" && "p.teaser").each(function (i, element) {
 
                 results.title = $(this).text();
                 results.link = $(this).parent().attr("href");
+                results.teaser = $(this).text();
 
-                if (results.title && results.link) {
+                if (results.title && results.link && results.teaser) {
                     db.Article.create(results)
                         .then(function (dbArticle) {
-                            console.log(dbArticle);
+                            // console.log(dbArticle);
                         })
                         .catch(function (err) {
                             console.log(err);
@@ -36,8 +36,30 @@ module.exports = function (app) {
                         )
                     console.log(results.title);
                     console.log(results.link);
+                    console.log(results.teaser);
+
                 }
             });
+
+            // $("p.teaser").each(function (i, element) {
+            //     results.teaser = $(this).text();
+            //     console.log(results.teaser);
+            //     db.Article.create(results)
+            //         .then(function (dbArticle) {
+            //             // console.log(dbArticle);
+            //         })
+            //         .catch(function (err) {
+            //             console.log(err);
+            //         },
+            //             function (err, created) {
+            //                 if (err) {
+            //                     console.log(err);
+            //                 } else {
+            //                     console.log(created);
+            //                 }
+            //             }
+            //         )
+            // })
         });
         res.send("Scrape Complete");
     });
