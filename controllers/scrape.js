cbef12f47bc925c0620ef9b3e22903ecf175e729
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-10-12 15:44:28 -0400

    scrape grabs title, teaser, and link then logs it successfully

---
+++ controllers/scrape.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const scrape = () => {
+
+    axios.get("https://www.npr.org").then(function (res) {
+
+        const $ = cheerio.load(res.data);
+        console.log("scraping...");
+        const results = [];
+
+        $("div.story-text").each(function (i, element) {
+
+            title = $(this).find("h3").text();
+            link = $(this).find("h3").parent().attr("href");
+            teaser = $(this).find("p").text();
+
+            if (title && link && teaser) {
+                const resObj = {
+                    title,
+                    teaser,
+                    link
+                };
+                results.push(resObj);
+            }
+
+        });
+        console.log(results);
+        return results;
+
+    });
+};
+
+module.exports = scrape;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,66 +1,67 @@
 const db = require("../models");
 const axios = require("axios");
-const cheerio = require("cheerio");
+// const cheerio = require("cheerio");
 // const mongoose = require("mongoose");
+const scrape = require("../controllers/scrape");
 
 module.exports = function (app) {
 
     app.get("/scrape", function (req, res) {
 
-        axios.get("https://www.npr.org").then(function (response) {
+        scrape();
+        // axios.get("https://www.npr.org").then(function (response) {
 
-            const $ = cheerio.load(response.data);
-            const results = [];
+        //     const $ = cheerio.load(response.data);
+        //     const results = [];
 
-            $("h3.title" && "p.teaser").each(function (i, element) {
+        //     $("h3.title" && "p.teaser").each(function (i, element) {
 
-                results.title = $(this).text();
-                results.link = $(this).parent().attr("href");
-                results.teaser = $(this).text();
+        //         results.title = $(this).text();
+        //         results.link = $(this).parent().attr("href");
+        //         results.teaser = $(this).text();
 
-                if (results.title && results.link && results.teaser) {
-                    db.Article.create(results)
-                        .then(function (dbArticle) {
-                            // console.log(dbArticle);
-                        })
-                        .catch(function (err) {
-                            console.log(err);
-                        },
-                            function (err, created) {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    console.log(created);
-                                }
-                            }
-                        )
-                    console.log(results.title);
-                    console.log(results.link);
-                    console.log(results.teaser);
+        //         if (results.title && results.link && results.teaser) {
+        //             db.Article.create(results)
+        //                 .then(function (dbArticle) {
+        //                     // console.log(dbArticle);
+        //                 })
+        //                 .catch(function (err) {
+        //                     console.log(err);
+        //                 },
+        //                     function (err, created) {
+        //                         if (err) {
+        //                             console.log(err);
+        //                         } else {
+        //                             console.log(created);
+        //                         }
+        //                     }
+        //                 )
+        //             console.log(results.title);
+        //             console.log(results.link);
+        //             console.log(results.teaser);
 
-                }
-            });
+        //         }
+        // });
 
-            // $("p.teaser").each(function (i, element) {
-            //     results.teaser = $(this).text();
-            //     console.log(results.teaser);
-            //     db.Article.create(results)
-            //         .then(function (dbArticle) {
-            //             // console.log(dbArticle);
-            //         })
-            //         .catch(function (err) {
-            //             console.log(err);
-            //         },
-            //             function (err, created) {
-            //                 if (err) {
-            //                     console.log(err);
-            //                 } else {
-            //                     console.log(created);
-            //                 }
-            //             }
-            //         )
-            // })
-        });
-        res.send("Scrape Complete");
+        // $("p.teaser").each(function (i, element) {
+        //     results.teaser = $(this).text();
+        //     console.log(results.teaser);
+        //     db.Article.create(results)
+        //         .then(function (dbArticle) {
+        //             // console.log(dbArticle);
+        //         })
+        //         .catch(function (err) {
+        //             console.log(err);
+        //         },
+        //             function (err, created) {
+        //                 if (err) {
+        //                     console.log(err);
+        //                 } else {
+        //                     console.log(created);
+        //                 }
+        //             }
+        //         )
+        // })
     });
-};
+    // res.send("Scrape Complete");
+}
